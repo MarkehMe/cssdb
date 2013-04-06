31b6cdf10df2f780c00802d0e964f2d6ba94c288
Rowan Manning <accounts@rowanmanning.co.uk>
2013-04-06 12:38:02 +0100

    Fix broken middle-clicking.
    Fixes #1.

--- public/script/app.js
+++ public/script/app.js
@@ -2,6 +2,19 @@
 (function (doc, host) {
     'use strict';
 
+    // Mouse button constants
+    var MOUSE_BUTTON_LEFT = 0;
+    var MOUSE_BUTTON_MIDDLE = 1;
+    var MOUSE_BUTTON_RIGHT = 2;
+
+    // Get clicked button from event (nasty, not super reliable)
+    function getClickedButton (evt) {
+        if (evt.button) {
+            return evt.button;
+        }
+        return MOUSE_BUTTON_LEFT;
+    }
+
     // Is a link external?
     function isLinkExternal (link) {
         return (link.indexOf(host) === -1 && link.match(/^https?:\/\//i));
@@ -10,11 +23,16 @@
     // Bind a click tracking event
     function bindClickTrackingEvent (link) {
         if (link.href && isLinkExternal(link.href)) {
-            link.onclick = function () {
-                if (_gaq && _gaq.push) {
+            link.onclick = function (evt) {
+                var button = getClickedButton(evt || window.event);
+                if (window._gaq && _gaq.push) {
                     _gaq.push(['_trackEvent', 'External Links', 'Click', link.href]);
                     setTimeout(function() {
-                        doc.location = link.href;
+                        if (button === MOUSE_BUTTON_LEFT) {
+                            doc.location = link.href;
+                        } else if (button === MOUSE_BUTTON_MIDDLE) {
+                            window.open(link.href);
+                        }
                     }, 100);
                     return false;
                 }
