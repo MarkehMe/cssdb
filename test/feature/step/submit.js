f00027156a26435564d06146109ae1708c8d3e53
Rowan Manning <accounts@rowanmanning.co.uk>
2013-05-09 22:19:22 +0100

    Write feature tests for submitting a library

---
+++ test/feature/step/submit.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// Step definitions
+module.exports = function () {
+    this.World = require('../support/world').World;
+
+    this.Given(/^the library "([^"]*)" has been added$/, function (libUrl, callback) {
+        var matches = libUrl.match(new RegExp(
+            '^https?:\\/\\/(www\\.)?github\\.com\\/' +
+            '([a-z0-9][a-z0-9\\-]{0,39})\\/([a-z0-9\\-\\.]{1,100})\\/?$',
+        'i'));
+        var owner = matches[2];
+        var name = matches[3];
+        this.db.collection('libraries').insert({
+            url: libUrl,
+            name: name,
+            owner: owner,
+            active: false,
+            notified: false,
+            key: 'test-activation-key'
+        }, function (err) {
+            if (err) { return callback.fail(err); }
+            callback();
+        });
+    });
+
+    this.When(/^I submit the submission form with "([^"]*)"$/i, function (libUrl, callback) {
+        this.browser
+            .fill('url', libUrl)
+            .pressButton('Submit Library', callback);
+    });
+
+    this.Then(/^the library "([^"]*)" should be added$/i, function (libUrl, callback) {
+        this.db.collection('libraries').findOne({url: libUrl}, function (err, repo) {
+            if (err) { return callback.fail(err); }
+            if (!repo) {
+                return callback.fail(new Error('Repository "' + libUrl + '" was not added'));
+            }
+            callback();
+        });
+    });
+
+    this.Then(/^the library "([^"]*)" should not be added$/i, function (libUrl, callback) {
+        this.db.collection('libraries').findOne({url: libUrl}, function (err, repo) {
+            if (err) { return callback.fail(err); }
+            if (repo) {
+                return callback.fail(new Error('Repository "' + libUrl + '" was added'));
+            }
+            callback();
+        });
+    });
+
+    this.Then(/^the library "([^"]*)" should be inactive$/i, function (libUrl, callback) {
+        this.db.collection('libraries').findOne({url: libUrl}, function (err, repo) {
+            if (err) { return callback.fail(err); }
+            if (!repo) {
+                return callback.fail(new Error('Repository "' + libUrl + '" was not added'));
+            }
+            if (repo.active) {
+                return callback.fail(new Error('Repository "' + libUrl + '" is active'));
+            }
+            callback();
+        });
+    });
+
+};
--- test/feature/submit.feature
+++ test/feature/submit.feature
@@ -4,7 +4,32 @@ Feature: Submit
     I want to be able to submit my CSS libraries to the site
     So that I can gain exposure for it and help out other designers/developers
 
-    Scenario: Submit a valid library
+    Scenario: View the submission page
         Given I am on the submit page
         Then I should see "Submit" in a heading
-        # Unfinished – run through submission process
+
+    Scenario: Submit a valid library
+        Given I am on the submit page
+        When I submit the submission form with "https://github.com/rowanmanning/cssdb"
+        Then I should see "Thanks"
+        And I should see "rowanmanning/cssdb"
+        And the library "https://github.com/rowanmanning/cssdb" should be added
+        And the library "https://github.com/rowanmanning/cssdb" should be inactive
+
+    Scenario: Submit a library which has already been added
+        Given I am on the submit page
+        And the library "https://github.com/rowanmanning/cssdb" has been added
+        When I submit the submission form with "https://github.com/rowanmanning/cssdb"
+        Then I should see "the library you entered has already been submitted"
+
+    Scenario: Submit a nonexistant library
+        Given I am on the submit page
+        When I submit the submission form with "https://github.com/rowanmanning/not-a-real-repo"
+        Then I should see "couldn't find the github repository for your library"
+        And the library "https://github.com/rowanmanning/not-a-real-repo" should not be added
+
+    Scenario: Submit a non-GitHub library
+        Given I am on the submit page
+        When I submit the submission form with "http://cssdb.co/"
+        Then I should see "please enter a valid github repository url"
+        And the library "http://cssdb.co/" should not be added
